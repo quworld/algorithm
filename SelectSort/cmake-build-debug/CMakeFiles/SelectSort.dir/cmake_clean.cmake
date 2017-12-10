4fb90f488b84979dd57b02bf30e29456448bbf6b
Hofoo <quworld@gmail.com>
2017-12-10 17:00:40 +0800

    add select sort

---
+++ SelectSort/cmake-build-debug/CMakeFiles/SelectSort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SelectSort.dir/main.cpp.o"
+  "SelectSort.pdb"
+  "SelectSort"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SelectSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
